53429fc7e6db18d46c0d84a88c3f66d46e498923
azevedo <azevedoantoniocicero@gmail.com>
2023-06-02 12:19:15 -0300

    melhorei a descrição

--- main.c
+++ main.c
@@ -8,28 +8,6 @@
  * Ele é capaz de criar processos para usuários.
  * O shell aceita comandos inseridos pelo usuário no prompt "$" e executa esses
  * comandos usando chamadas do sistema fornecidas pela biblioteca padrão do C.
- *
- * Casos Tratados:
- * $ prog
- * Cria um processo em foreground deste programa.
- *
- * $ prog &
- * Cria um processo em background deste programa.
- *
- * $ prog parâmetros
- * Cria um processo em foreground deste programa com parâmetros especificados.
- *
- * $ prog parâmetros &
- * Cria um processo em background deste programa com parâmetros especificados.
- *
- * $ prog [parâmetros] > arquivo
- * Cria um processo deste programa com parâmetros especificados e redireciona a
- * saída gerada para um arquivo.
- *
- * $ prog1 | prog2
- * Cria um processo do programa 1 que envia a saída gerada para a entrada de um
- * processo criado do programa 2.
-
  */
 #include "stdio.h"
 #include "stdlib.h"
