efae29859fd0b4dec08cd70a1a8949fe0456dba1
azevedo <azevedoantoniocicero@gmail.com>
2023-06-03 02:48:38 -0300

    promp puro + leitor de string

--- main.c
+++ main.c
@@ -9,14 +9,46 @@
  * O shell aceita comandos inseridos pelo usuário no prompt "$" e executa esses
  * comandos usando chamadas do sistema fornecidas pela biblioteca padrão do C.
  */
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 
-int main(void){
-    /*
-       */
-  return (0);
+void shell_loop();
+char* inserir_comando();
+char** split_comando();
+
+
+int main(void)
+{
+    shell_loop();
+    return (0);
 }
 
+void shell_loop()
+{
+    char* comando = NULL;
+    while(true)
+    {
+        printf("$ ");
+        comando = inserir_comando();
+    }
+}
 
+char* inserir_comando()
+{
+    char *comando= NULL;
+    size_t size = 0;
+    size_t ok;
+    ok = getline(&comando, &size, stdin);
+    if(ok == -1)
+    {
+        printf("erro");
+        exit(1);
+    }
+    return comando;
+}
+char** split_comando()
+{
+
+}
